de720fc8745b7819101623bc375a19b48958fd7f
ksj01 <T1smyipo>
2017-04-07 15:49:53 -0600

    Fleshed out the rest of the tutorial, added glut.h, added OpenGL stuff from http://www.multigesture.net/articles/how-to-draw-pixels-to-a-texture-opengl/

--- chip8.cpp
+++ chip8.cpp
@@ -2,15 +2,48 @@
 //
 
 //#include "stdafx.h"
-//#include OpenGL graphics and input stuff
+#include <stdio.h>
+#include <glut.h>
 #include "chip8.h"
 
-chip8 myChip8;
+// Display size
+#define SCREEN_WIDTH 64
+#define SCREEN_HEIGHT 32
 
-int main()
+chip8 myChip8;
+int modifier = 10;
+
+// Window size
+int display_width = SCREEN_WIDTH * modifier;
+int display_height = SCREEN_HEIGHT * modifier;
+
+void display();
+void reshape_window(GLsizei w, GLsizei h);
+
+// Use new drawing method
+typedef unsigned __int8 u8;
+u8 screenData[SCREEN_HEIGHT][SCREEN_WIDTH][3];
+void setupTexture();
+
+int main(int argc, char **argv)
 {
 	//set up render systems and register input callbacks
 	//setupGraphics();
+	// Setup OpenGL
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+	glutInitWindowSize(display_width, display_height);
+	glutInitWindowPosition(320, 320);
+	glutCreateWindow("myChip8 by Laurence Muller");
+
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
+	glutReshapeFunc(reshape_window);
+
+	setupTexture();
+
+	glutMainLoop();
 	//setupInput();
 
 	//initialize the chip8 system and load the game into memory
@@ -33,3 +66,78 @@ int main()
     return 0;
 }
 
+// Setup Texture
+void setupTexture()
+{
+	// Clear screen
+	for (int y = 0; y < SCREEN_HEIGHT; ++y)
+		for (int x = 0; x < SCREEN_WIDTH; ++x)
+			screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 0;
+
+	// Create a texture 
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, SCREEN_WIDTH, SCREEN_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)screenData);
+
+	// Set up the texture
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+	// Enable textures
+	glEnable(GL_TEXTURE_2D);
+}
+
+void updateTexture(const chip8& c8)
+{
+	// Update pixels
+	for (int y = 0; y < 32; ++y)
+		for (int x = 0; x < 64; ++x)
+			if (c8.gfx[(y * 64) + x] == 0)
+				screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 0;	// Disabled
+			else
+				screenData[y][x][0] = screenData[y][x][1] = screenData[y][x][2] = 255;  // Enabled
+
+																						// Update Texture
+	glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)screenData);
+
+	glBegin(GL_QUADS);
+	glTexCoord2d(0.0, 0.0);		glVertex2d(0.0, 0.0);
+	glTexCoord2d(1.0, 0.0); 	glVertex2d(display_width, 0.0);
+	glTexCoord2d(1.0, 1.0); 	glVertex2d(display_width, display_height);
+	glTexCoord2d(0.0, 1.0); 	glVertex2d(0.0, display_height);
+	glEnd();
+}
+
+void display()
+{
+	myChip8.emulateCycle();
+
+	if (myChip8.drawFlag)
+	{
+		// Clear framebuffer
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Draw pixels to texture
+		updateTexture(myChip8);
+
+		// Swap buffers!
+		glutSwapBuffers();
+
+		// Processed frame
+		myChip8.drawFlag = false;
+	}
+}
+
+void reshape_window(GLsizei w, GLsizei h)
+{
+	glClearColor(0.0f, 0.0f, 0.5f, 0.0f);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, w, h, 0);
+	glMatrixMode(GL_MODELVIEW);
+	glViewport(0, 0, w, h);
+
+	// Resize quad
+	display_width = w;
+	display_height = h;
+}
--- chip8.h
+++ chip8.h
@@ -1,5 +1,6 @@
 #include <string>
 #include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
@@ -10,15 +11,34 @@ private:
 	unsigned char V[16];
 	unsigned short I;
 	unsigned short pc;
-	unsigned char gfx[64 * 32];
 	unsigned char delay_timer;
 	unsigned char sound_timer;
 	unsigned short stack[16];
 	unsigned short sp;
 	unsigned char key[16];
+	unsigned char chip8_fontset[80] =
+	{
+		0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+		0x20, 0x60, 0x20, 0x20, 0x70, // 1
+		0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+		0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+		0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+		0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+		0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+		0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+		0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+		0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+		0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+		0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+		0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+		0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+		0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+		0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+	};
 
 public:
 	bool drawFlag;
+	unsigned char gfx[64 * 32];
 
 	void initialize() {
 		//initalize registers and memory once
@@ -26,7 +46,7 @@ public:
 		opcode = 0;	// Reset current opcode
 		I = 0;		// Reset index register
 		sp = 0;		// Reset stack pointer
-		//Clear Display
+		///Clear Display
 		//Clear Stack
 		for (int i = 0; i < 16; ++i) {
 			stack[i] = NULL;
@@ -50,7 +70,7 @@ public:
 	}
 
 	void loadGame(string name) {
-
+		
 	}
 
 	void emulateCycle() {
@@ -58,9 +78,84 @@ public:
 		opcode = memory[pc] << 8 | memory[pc + 1];
 		//decode opcode
 			//decode the opcode from the opcode table on Wikipedia.
-		//execute opcode
+		switch (opcode & 0xF000) {
+			// swtiches for all opcodes //
+		case 0xA000: // ANNN: Sets I to the address NNN
+			// Execute opcode //
+			I = opcode & 0x0FFF;
+			pc += 2;
+			break;
+			// More cases and opcodes //
 
+		case 0x0000:	//0x00E0: Clears the screen
+			//Execute opcode
+			break;
+
+		case 0x000E:	//0x00EE: Returns from subroutine
+			//Execute opcode
+			break;
+
+		case 0x2000:
+			stack[sp] = pc;
+			++sp;
+			pc = opcode & 0x0FFF;
+			break;
+		case 0x0004:
+			if (V[(opcode & 0x00F0) >> 4] > (0xFF - V[(opcode & 0x0F00) >> 8])) {
+				V[0xF] = 1; //carry
+			}
+			else {
+				V[0xF] = 0;
+			}
+			V[(opcode & 0x0F00) >> 8] += V[(opcode & 0x00F0) >> 4];
+			pc += 2;
+			break;
+
+		case 0x0033:
+			memory[I] = V[(opcode & 0x0F00) >> 8] / 100;
+			memory[I + 1] = (V[(opcode & 0x0F00) >> 8] / 10) % 10;
+			memory[I + 2] = (V[(opcode & 0x0F00) >> 8] % 100) % 10;
+			pc += 2;
+			break;
+
+		case 0xD000:
+		{
+			unsigned short x = V[(opcode & 0x0F00) >> 8];
+			unsigned short y = V[(opcode & 0x00F0) >> 4];
+			unsigned short height = opcode & 0x000F;
+			unsigned short pixel;
+
+			V[0xF] = 0;
+			for (int yline = 0; yline < height; yline++) {
+				pixel = memory[I + yline];
+				for (int xline = 0; xline < 8; xline++) {
+					if ((pixel & (0x80 >> xline)) != 0) {
+						if (gfx[(x + xline + ((y + yline) * 64))] == 1) {
+							V[0xF] = 1;
+						}
+						gfx[x + xline + ((y + yline) * 64)] ^= 1;
+					}
+				}
+			}
+			drawFlag = true;
+			pc += 2;
+		}
+			break;
+
+		default:
+			cout << "Unknown opcode: 0x" << opcode << endl;
+		}
 		//update timers
+
+		if (delay_timer > 0) {
+			--delay_timer;
+		}
+		if (sound_timer > 0) {
+			if (sound_timer == 1) {
+				cout << "BEEP!" << endl;
+			}
+			--sound_timer;
+		}
 	}
 
 	void setKeys() {
