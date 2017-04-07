7416b707c7a5f111dcea202cd27bb41bcdfc1d81
ksj01 <T1smyipo>
2017-04-06 21:38:33 -0600

    Some psuedocode stuff.

--- chip8.cpp
+++ chip8.cpp
@@ -2,11 +2,26 @@
 //
 
 //#include "stdafx.h"
+//#include OpenGL graphics and input stuff
 #include "chip8.h"
 
+chip8 myChip8;
 
 int main()
 {
+	//set up render systems and register input callbacks
+
+	//initialize the chip8 system and load the game into memory
+
+	//emulation loop {
+
+		//emulate one cycle
+
+		//If the draw flag is set, update the screen
+
+		// Store key press state (press and release)
+
+	// }
     return 0;
 }
 
