7fde2997356839cc2d9e5327f7496252dfada774
ksj01 <T1smyipo>
2017-04-06 22:44:38 -0600

    Updated. Most of initialize() has been fleshed out.

--- chip8.cpp
+++ chip8.cpp
@@ -10,18 +10,26 @@ chip8 myChip8;
 int main()
 {
 	//set up render systems and register input callbacks
+	//setupGraphics();
+	//setupInput();
 
 	//initialize the chip8 system and load the game into memory
+	myChip8.initialize();
+	myChip8.loadGame("pong");
 
 	//emulation loop {
-
+	for (;;)
+	{
 		//emulate one cycle
-
+		myChip8.emulateCycle();
 		//If the draw flag is set, update the screen
-
+		if (myChip8.drawFlag) {
+			drawGraphics();
+		}
 		// Store key press state (press and release)
-
+		myChip8.setKeys();
 	// }
+	}
     return 0;
 }
 
--- chip8.h
+++ chip8.h
@@ -1,6 +1,10 @@
+#include <string>
+#include <stdio.h>
+
+using namespace std;
 
 class chip8 {
-
+private:
 	unsigned short opcode;
 	unsigned char memory[4096];
 	unsigned char V[16];
@@ -12,5 +16,55 @@ class chip8 {
 	unsigned short stack[16];
 	unsigned short sp;
 	unsigned char key[16];
+
+public:
+	bool drawFlag;
+
+	void initialize() {
+		//initalize registers and memory once
+		pc = 0x200;	// program counter starts at 0x200
+		opcode = 0;	// Reset current opcode
+		I = 0;		// Reset index register
+		sp = 0;		// Reset stack pointer
+		//Clear Display
+		//Clear Stack
+		for (int i = 0; i < 16; ++i) {
+			stack[i] = NULL;
+		}
+		//Clear Registers V0-VF
+		for (int i = 0; i < 16; ++i) {
+			V[i] = NULL;
+		}
+		//Clear memory
+		for (int i = 0; i < 4096; ++i) {
+			memory[i] = NULL;
+		}
+
+		//Load fontset
+		for (int i = 0; i < 80; ++i) {
+			memory[i] = chip8_fontset[i];
+		}
+
+		delay_timer = 0;	//Reset timers
+		sound_timer = 0;
+	}
+
+	void loadGame(string name) {
+
+	}
+
+	void emulateCycle() {
+		//fetch opcode
+		opcode = memory[pc] << 8 | memory[pc + 1];
+		//decode opcode
+			//decode the opcode from the opcode table on Wikipedia.
+		//execute opcode
+
+		//update timers
+	}
+
+	void setKeys() {
+
+	}
 
 };
